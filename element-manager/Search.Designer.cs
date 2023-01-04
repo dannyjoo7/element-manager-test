b49b9df6a89d5c620d44587cf210231e30bf9818
socra167@gmail.com <socra167@gmail.com>
2023-01-04 13:59:45 +0900

    fix: 검색 텍스트박스 디자인 수정

--- element-manager/Search.Designer.cs
+++ element-manager/Search.Designer.cs
@@ -31,7 +31,9 @@
             this.iconPictureBox2 = new FontAwesome.Sharp.IconPictureBox();
             this.textUpdateName = new System.Windows.Forms.TextBox();
             this.comboSearch = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // iconPictureBox2
@@ -41,10 +43,10 @@
             this.iconPictureBox2.IconChar = FontAwesome.Sharp.IconChar.MagnifyingGlass;
             this.iconPictureBox2.IconColor = System.Drawing.Color.Black;
             this.iconPictureBox2.IconFont = FontAwesome.Sharp.IconFont.Solid;
-            this.iconPictureBox2.IconSize = 40;
+            this.iconPictureBox2.IconSize = 28;
             this.iconPictureBox2.Location = new System.Drawing.Point(0, 0);
             this.iconPictureBox2.Name = "iconPictureBox2";
-            this.iconPictureBox2.Size = new System.Drawing.Size(40, 40);
+            this.iconPictureBox2.Size = new System.Drawing.Size(34, 28);
             this.iconPictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.iconPictureBox2.TabIndex = 11;
             this.iconPictureBox2.TabStop = false;
@@ -53,10 +55,10 @@
             // 
             // textUpdateName
             // 
-            this.textUpdateName.Font = new System.Drawing.Font("맑은 고딕", 18.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textUpdateName.Location = new System.Drawing.Point(40, 0);
+            this.textUpdateName.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textUpdateName.Location = new System.Drawing.Point(34, 1);
             this.textUpdateName.Name = "textUpdateName";
-            this.textUpdateName.Size = new System.Drawing.Size(610, 40);
+            this.textUpdateName.Size = new System.Drawing.Size(673, 27);
             this.textUpdateName.TabIndex = 14;
             this.textUpdateName.Text = "검색어 입력";
             this.textUpdateName.TextChanged += new System.EventHandler(this.textUpdateName_TextChanged);
@@ -64,8 +66,7 @@
             // comboSearch
             // 
             this.comboSearch.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.comboSearch.Font = new System.Drawing.Font("맑은 고딕", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboSearch.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.comboSearch.FormattingEnabled = true;
             this.comboSearch.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.comboSearch.Items.AddRange(new object[] {
@@ -73,12 +74,20 @@
             "게시자",
             "회사명",
             "태그명"});
-            this.comboSearch.Location = new System.Drawing.Point(650, 0);
+            this.comboSearch.Location = new System.Drawing.Point(707, 0);
             this.comboSearch.Name = "comboSearch";
-            this.comboSearch.Size = new System.Drawing.Size(150, 40);
+            this.comboSearch.Size = new System.Drawing.Size(93, 28);
             this.comboSearch.TabIndex = 15;
             this.comboSearch.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-            this.comboSearch.SelectedIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Controls.Add(this.iconPictureBox2);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(34, 28);
+            this.panel1.TabIndex = 16;
             // 
             // Search
             // 
@@ -86,14 +95,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(800, 550);
-            this.Controls.Add(this.comboSearch);
             this.Controls.Add(this.textUpdateName);
-            this.Controls.Add(this.iconPictureBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.comboSearch);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Search";
             this.Text = "Search";
             this.Load += new System.EventHandler(this.Search_Load);
             ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -104,5 +114,6 @@
         private FontAwesome.Sharp.IconPictureBox iconPictureBox2;
         private TextBox textUpdateName;
         private ComboBox comboSearch;
+        private Panel panel1;
     }
 }
