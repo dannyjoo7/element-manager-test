0b46aed0fd8bc14bb6771fa1faafa14b45cb0987
socra167@gmail.com <socra167@gmail.com>
2023-01-11 15:42:46 +0900

    docs: user 더미데이터 추가 SQL

---
+++ dummy.sql
@@ -0,0 +1,3 @@
+INSERT INTO pbl.`user`
+(user_name, user_id, user_pw, user_sex, user_email, user_phone, user_addr, introduce, op_check, user_state)
+VALUES('test', 'test', 'passwd', 'man', 'test@test', '1234-1234', 'test', 'test', 2, '정상');
