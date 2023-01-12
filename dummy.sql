fb4195d6b55ce5f5c5f97aea7e221a037b1fd5af
socra167@gmail.com <socra167@gmail.com>
2023-01-12 11:00:05 +0900

    docs: User 더미 데이터 수정

--- dummy.sql
+++ dummy.sql
@@ -1,3 +1,3 @@
 INSERT INTO pbl.`user`
 (user_name, user_id, user_pw, user_sex, user_email, user_phone, user_addr, introduce, op_check, user_state)
-VALUES('test', 'test', 'passwd', 'man', 'test@test', '1234-1234', 'test', 'test', 2, '정상');
+VALUES('testname', 'test', 'passwd', 'man', 'testaddress@gmail.com', '01012341234', 'korea', 'This is introduce', 2, '정상');
